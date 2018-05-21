a98673c08a29d33c591c94f7feebedfa4d2c916b
Evan duBois <duboisevan@gmail.com>
2018-05-21 13:26:54 -0400

    added google api key back in

--- app/javascript/components/RaceRegister.js
+++ app/javascript/components/RaceRegister.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const RaceRegister = props => {
-  const map = `https://maps.googleapis.com/maps/api/staticmap?center=${props.race.street},${props.race.city},${props.race.state}&zoom=16&size=400x400&markers=color:red%7C${props.race.street},${props.race.city},${props.race.state}`
+  const map = `https://maps.googleapis.com/maps/api/staticmap?center=${props.race.street},${props.race.city},${props.race.state}&zoom=16&size=400x400&markers=color:red%7C${props.race.street},${props.race.city},${props.race.state}&key=AIzaSyDKXWUi5QTlNwpafPbvqtGKNl7APMk0atE`
   const directions = `https://www.google.com/maps/dir//${props.race.street},${props.race.city},${props.race.state}`
   return (
     <div>
