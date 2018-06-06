159de31c6b4eadd3f25257c91e3bf0661fd46576
Evan duBois <duboisevan@gmail.com>
2018-06-05 22:41:16 -0400

    my save line is messing things up. check that I'm using the right params

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -10,9 +10,17 @@ class Api::V1::RegistrationsController < ApplicationController
   end
 
   def create
-    Registration.create!(race_id: params["race_id"], user: current_user)
-    if !params["joinTeam"].nil?
-      Roster.create!(user: current_user, team_id: params["joinTeam"])
+    binding.pry
+    if Roster.find_by(team_id: params["race_id"], user: current_user)
+      binding.pry
+      editRoster = Roster.find_by(team_id: params["race_id"], user: current_user)
+      editRoster.team_id = params["joinTeam"]
+      editRoster.save!
+    else
+      Registration.create!(race_id: params["race_id"], user: current_user)
+      if !params["joinTeam"].nil?
+        Roster.create!(user: current_user, team_id: params["joinTeam"])
+      end
     end
 
     render json: Race.find(params['race_id'])
